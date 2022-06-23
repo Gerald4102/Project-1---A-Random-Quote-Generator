2b75240c588f45508bb35b88be167fe6f9343f91
Gerald4102 <106319833+Gerald4102@users.noreply.github.com>
2022-06-23 16:06:09 +0100

    Added comments

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -7,65 +7,65 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
+
+// several quotes as objects in an arrary
 const quotes = [
   {
-    quote: 'a',
-    source: 'asource',
-    citation: 'acit',
-    year: '2000',
+    quote: 'your relationship with change will define your life. if you accept it, it will inspire you to be more present and to live without holding back.',
+    source: 'yung pueblo',
+    citation: 'Instagram',
+    year: '2021',
     tag: 'wellbeing'
   },
   {
-    quote: 'b',
-    source: 'bsource',
-    citation: 'bcit',
+    quote: 'Do not be afraid to love. Without love, life is impossible',
+    source: 'Thich Nhat Hanh',
+    tag: 'wellbeing'
+  },
+  {
+    quote: "Don't count the days. Make the days count.",
+    source: 'Muhammad Ali',
+    tag: 'inspirational'
+  },
+  {
+    quote: 'May the force be with you',
+    source: 'various',
+    citation: 'Star Wars',
+    year: '1977',
+    tag: 'movies'
+  },
+  {
+    quote: "You're a wizard, Harry",
+    source: 'Hagrid',
+    citation: "Harr Potter and the Philosopher's Stone",
     year: '2001',
     tag: 'movies'
   },
   {
-    quote: 'c',
-    source: 'csource'
-  },
-  {
-    quote: 'd',
-    source: 'dsource',
-    citation: 'dcit',
-    year: '2003'
-  },
-  {
-    quote: 'e',
-    source: 'esource'
-  },
-  {
-    quote: 'f',
-    source: 'fsource'
+    quote: 'At times, our own light goes out and is rekindled by a spark from another person. Each of us has cause to think with deep gratitude of those who have lighted the flame within us.',
+    source: 'Albert Schweitzer',
+    tag: 'inspirational'
   }
 ];
 
-/***
- * `getRandomQuote` function
-***/
+// Function to generate a random number no that is no higher than the number of quotes in the array
+// then return the quote equal to that random number from the array
 function getRandomQuote() {
   let randomNumber = Math.floor(Math.random() * quotes.length);
   let randomQuote = quotes[randomNumber]
   return randomQuote;
 }
-//console.log(getRandomQuote());
 
-/***
- * `printQuote` function
-***/
+
+//Function that will build the HTML from the quote fetched from the array
 function printQuote() {
   
   let quoteToPrint = getRandomQuote()
   let htmlToPrint = '';
 
     htmlToPrint += `<p class="quote"> ${quoteToPrint.quote} </p>`;
-    // Question: are there supposed to be line breaks for the HTML?
     htmlToPrint += `<p class="source"> ${quoteToPrint.source}`;
+    // conditional statements to check if optional properties of the quotes exist
       if( quoteToPrint.citation !== undefined) {
         htmlToPrint += `<span class="citation"> ${quoteToPrint.citation} </span>`; 
       }
@@ -81,18 +81,18 @@ function printQuote() {
   changeBackground();
 }
 
-
-// https://alvarotrigo.com/blog/change-css-javascript/
+// Function to generate random background colours using rgba() method
 function changeBackground() {
   let red = Math.floor(Math.random() * 256);
   let green = Math.floor(Math.random() * 256);
   let blue = Math.floor(Math.random() * 256);
-  let alpha = Math.random() + 0.01;
+  let alpha = Math.random();
+// element variable adapted from source: https://alvarotrigo.com/blog/change-css-javascript/
   let element = document.querySelector('body');
-  element.style.backgroundColor = `rgba(${red},${green},${blue},1)`;
+  element.style.backgroundColor = `rgba(${red},${green},${blue},${alpha})`;
 }
 
-// timer to change quote every 10 seconds
+// Arrow function timer to change quote every 10 seconds
 let autoChange = () => refresh = setInterval(printQuote, 10000);
 autoChange()
 
